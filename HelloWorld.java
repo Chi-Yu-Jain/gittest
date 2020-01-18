5819b7448c413ca76347f73c793681fcbb135cbf
HIHI <tyai013441@gmail.com>
2020-01-18 15:03:27 +0800

    test1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,10 @@ package xxx;
 
 public class HelloWorld 
 {
-	//1
+	
 	public static void main(String[] args) 
 	{
+	int X =0;
 		String a1=("2"),a2=("3"),a3=("4");
 		System.out.println(a1+a2+a3);
 	}
