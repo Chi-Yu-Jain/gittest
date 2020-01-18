81d5cabdc8e69cd7b4e56fa2c847bf591142104b
HIHI <tyai013441@gmail.com>
2020-01-18 13:15:28 +0800

    init commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package xxx;
+
+public class HelloWorld 
+{
+//	int a1=1;
+	public static void main(String[] args) 
+	{
+		String a1=("1"),a2=("2"),a3=("3");
+		System.out.println(a1+a2+a3);
+	}
+}
